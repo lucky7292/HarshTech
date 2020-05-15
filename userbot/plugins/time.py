d9896505aeef61f77341dfa880b64bd341f62c1d
HarshTech26 <58262783+HarshTech26@users.noreply.github.com>
2020-05-15 21:54:20 +0300

    Update time.py

--- userbot/plugins/time.py
+++ userbot/plugins/time.py
@@ -36,7 +36,7 @@ async def _(event):
     await borg.send_file(  # pylint:disable=E0602
         event.chat_id,
         required_file_name,
-        caption="Userbot: Powered by @pureindialover",
+        caption="Userbot: Powered by @anon_cracker",
         # Courtesy: @ManueI15
         reply_to=reply_msg_id
     )
