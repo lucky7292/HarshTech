e85b67ff9fee24cf0b66a43d3a9b0f1b3d799056
HarshTech26 <58262783+HarshTech26@users.noreply.github.com>
2020-05-15 21:49:01 +0300

    Update pmpermit_menu.py

--- userbot/plugins/pmpermit_menu.py
+++ userbot/plugins/pmpermit_menu.py
@@ -3,7 +3,7 @@
 Support chatbox for pmpermit.
 Used by incoming messages with trigger as /start
 Will not work for already approved people.
-Credits: written by Indian Bhai {@pureindialover}
+Credits: written by AnonCracker {@anon_cracker}
 """
 import asyncio
 import io 
